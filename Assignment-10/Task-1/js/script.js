8894f614b113e6ffe347ea31ba68712b8c1bcf69
sukh8661 <sukhpreet8661@gmail.com>
2024-11-20 20:26:17 +0530

    Add files via upload

---
+++ Assignment-10/Task-1/js/script.js
@@ -0,0 +1,16 @@
+var Name, age, salary, designation, city, department;
+
+Name = "Sukhpreet Singh";
+age = "AGE : 25";
+salary = "SALARY : 80000";
+designation = "DESIGNATION : Manager";
+department = "DEPARTMENT : Sales Department";
+city = "CITY : Jalandhar";
+
+// js used to update the values 
+document.getElementById("name").innerHTML = Name;
+document.getElementById("age").innerHTML = age;
+document.getElementById("salary").innerHTML = salary;
+document.getElementById("designation").innerHTML = designation;
+document.getElementById("department").innerHTML = department;
+document.getElementById("city").innerHTML = city;
